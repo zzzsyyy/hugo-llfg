406de5759a8d2ca7c0027e349c196dcb6750408d
zzzsyyy <me@zzzsy.top>
2023-04-06 17:43:35 +0800

    feat: add toc feat: add sticktoc style: change grid

---
+++ assets/js/toc.js
@@ -0,0 +1,23 @@
+function activateNavElementOnScroll() {
+    const headers = document.querySelectorAll('.markdown h1, .markdown h2, .markdown h3, .markdown h4, .markdown h5, .markdown h6');
+    const headerLinks = document.querySelectorAll('.post_toc li a');
+    const headerLinksArray = Array.from(headerLinks);
+  
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY;
+      
+      headers.forEach(header => {
+        if (header.offsetTop - 300 < scrollY) {
+          headerLinksArray.forEach(link => {
+            link.classList.remove('active');
+            
+            const href = link.getAttribute('href');
+            if (header.getAttribute('id') === href.slice(1)) {
+              link.classList.add('active');
+            }
+          })
+        }
+      });
+    });
+  }
+  
