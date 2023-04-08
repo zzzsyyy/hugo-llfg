f155c85e3367405bc3737c9726f07ae787b0ea6c
zzzsyyy <me@zzzsy.top>
2023-04-08 18:00:14 +0800

    feat: add back-to-top refactor: no complier sass style: add {reset, prism, [partoals], [pages], global...} feat: add render-link markup

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,23 +1,22 @@
 function activateNavElementOnScroll() {
-    const headers = document.querySelectorAll('.markdown h1, .markdown h2, .markdown h3');
-    const headerLinks = document.querySelectorAll('.post_toc li a');
-    const headerLinksArray = Array.from(headerLinks);
-  
-    window.addEventListener('scroll', () => {
-      const scrollY = window.scrollY;
-      
-      headers.forEach(header => {
-        if (header.offsetTop - 300 < scrollY) {
-          headerLinksArray.forEach(link => {
-            link.classList.remove('active');
-            
-            const href = link.getAttribute('href');
-            if (header.getAttribute('id') === href.slice(1)) {
-              link.classList.add('active');
-            }
-          })
-        }
-      });
+  const headers = document.querySelectorAll('.markdown h1, .markdown h2, .markdown h3');
+  const headerLinks = document.querySelectorAll('.post-toc li a');
+  const headerLinksArray = Array.from(headerLinks);
+
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    
+    headers.forEach(header => {
+      if (header.offsetTop - 300 < scrollY) {
+        headerLinksArray.forEach(link => {
+          link.classList.remove('active');
+          
+          const href = link.getAttribute('href');
+          if (header.getAttribute('id') === href.slice(1)) {
+            link.classList.add('active');
+          }
+        })
+      }
     });
-  }
-  
+  });
+}
